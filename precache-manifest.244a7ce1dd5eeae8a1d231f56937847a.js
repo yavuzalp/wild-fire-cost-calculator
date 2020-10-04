54b8c7bc0a0c2d6f5a9cd21246e319c4ea4c6f9b
Alp <turkoglu.yavuzalp@gmail.com>
2020-10-03 17:51:16 -0700

    Updates

---
+++ precache-manifest.244a7ce1dd5eeae8a1d231f56937847a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7195120199965eb70466297378b07dea",
+    "url": "/wild-fire-cost-calculator/index.html"
+  },
+  {
+    "revision": "dd3a626547300466b33a",
+    "url": "/wild-fire-cost-calculator/static/css/2.b0618883.chunk.css"
+  },
+  {
+    "revision": "268f0c69e05bf39738e1",
+    "url": "/wild-fire-cost-calculator/static/css/main.62bc0c7f.chunk.css"
+  },
+  {
+    "revision": "dd3a626547300466b33a",
+    "url": "/wild-fire-cost-calculator/static/js/2.5d86d13c.chunk.js"
+  },
+  {
+    "revision": "0fb0e672f1308cb5b7677cdb93fc42ab",
+    "url": "/wild-fire-cost-calculator/static/js/2.5d86d13c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "268f0c69e05bf39738e1",
+    "url": "/wild-fire-cost-calculator/static/js/main.1056f697.chunk.js"
+  },
+  {
+    "revision": "aaf8fd69b3b8126efc08",
+    "url": "/wild-fire-cost-calculator/static/js/runtime-main.1dc55a97.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/wild-fire-cost-calculator/static/media/logo.5d5d9eef.svg"
+  }
+]);
